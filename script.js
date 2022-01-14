a3781457d312c8754d9e9b82107cf61f485fd918
annaesther <annaesther2911@gmail.com>
2022-01-14 10:53:42 -0300

    trabalho finalizado

---
+++ script.js
@@ -0,0 +1,20 @@
+// Crie uma variável que receba um nome;
+let name = "Anna";
+
+// Crie uma variável que receba um número;
+let number = 10;
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+let year = 2022 - 10;
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+console.log(number + year);
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+console.log("Estou mexendo no console.log");
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+let quartaFeira = false;
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log("O dia da semana quarta-feira pertence ao final de semana? R: quartaFeira");
